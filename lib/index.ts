218b46eaa1df6498f09bb0ee7520b605e71d1346
Guilherme Oenning <guilherme.o@malwee.com.br>
2016-01-14 14:01:01 -0200

    including refactors

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,21 @@
 import {Assertion} from './Assertion';
 import {ensure} from './ensure';
 import {Guard} from './Guard';
+import {ValidationRule} from './ValidationRule';
+import {ValidationResult, ValidationFailure} from './ValidationResult';
+import {Validator} from './validators/Validator';
+import {EqualValidator} from './validators/EqualValidator';
+import {LengthValidator} from './validators/LengthValidator';
+import {ValidationContext} from './ValidationContext';
 
 export {
   Assertion,
   ensure,
-  Guard
+  Guard,
+  ValidationRule,
+  ValidationResult,
+  ValidationFailure,
+  EqualValidator,
+  LengthValidator,
+  ValidationContext
 }
