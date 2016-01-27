b5d8a3d0248a7d96f1dd512a1ef317ddff8a575e
goenning <oenning.ti@gmail.com>
2016-01-27 07:51:41 -0200

    deleteing typings from repo

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser\ambient\assertion-error\assertion-error.d.ts" />
-/// <reference path="browser\ambient\chai\chai.d.ts" />
-/// <reference path="browser\ambient\lodash\lodash.d.ts" />
-/// <reference path="browser\ambient\mocha\mocha.d.ts" />
-/// <reference path="browser\ambient\node\node.d.ts" />
